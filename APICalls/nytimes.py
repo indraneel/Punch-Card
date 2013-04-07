4dbb2740f54b069271452dd32bce9ab21c9fe3e5
Indraneel Purohit <i.m.indraneel@gmail.com>
2013-04-07 05:51:33 -0400

    passed in data from nytimespy to html like a baus but im tired

--- APICalls/nytimes.py
+++ APICalls/nytimes.py
@@ -1,6 +1,7 @@
 import httplib
 import simplejson as json
 import pystache 
+import webbrowser
 
 def get_stories(input):
     query = input 
@@ -12,8 +13,18 @@ def get_stories(input):
     return json.loads(data)
 
 def generate_template(d):
-    print pystache.render('Date {{date}}!', d) 
-
+    fo = open('../NYTelegram/index.html', 'rb')
+    n = pystache.render(fo.read(),d)
+    fo.close()
+    out = open('../NYTelegram/output.html', 'wb+')
+    out.write(n)
+    out.close()
+    webbrowser.open('file:///Users/indraneelpurohit/Dropbox/Punch-Card/NYTelegram/output.html')
+    #fo = open('layout.html', 'w+')
+    #fo.write("<!DOCTYPE html>\n")
+    #fo.write("<html>")
+    #fo.write(pystache.render('Date {{date}}!', d))
+    #fo.close();
 
 d = get_stories("north%20korea")
 generate_template(d['results'][0])
