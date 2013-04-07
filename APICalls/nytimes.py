d9b0222fb82add2441583e4e87ec6cca19d2afb2
Indraneel Purohit <i.m.indraneel@gmail.com>
2013-04-07 04:45:28 -0400

    working json handling

--- APICalls/nytimes.py
+++ APICalls/nytimes.py
@@ -1,10 +1,19 @@
 import httplib
 import simplejson as json
+import pystache 
 
-query = "bailout"
-conn = httplib.HTTPConnection("api.nytimes.com")
-url = "/svc/search/v1/article?query=" + query + "&api-key=ddabadc6f489ae99ad087f1892088d43:7:67526141"
-conn.request("GET", url)
-r = conn.getresponse()
-data = r.read()
-print data
+def get_stories(input):
+    query = input 
+    conn = httplib.HTTPConnection("api.nytimes.com")
+    url = "/svc/search/v1/article?query=" + query + "&api-key=ddabadc6f489ae99ad087f1892088d43:7:67526141"
+    conn.request("GET", url)
+    r = conn.getresponse()
+    data = r.read()
+    return json.loads(data)
+
+def generate_template(d):
+    print pystache.render('Date {{date}}!', d) 
+
+
+d = get_stories("north%20korea")
+generate_template(d['results'][0])
