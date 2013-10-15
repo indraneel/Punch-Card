8319062390e596a17e678bf4f865a14b02b3c358
Robert Fidler <robertf224@yahoo.com>
2013-10-14 23:55:18 -0400

    slight update

--- Email/send_email.py
+++ Email/send_email.py
@@ -1,13 +1,13 @@
 #Sending an email through gmail using Python
 import smtplib
 # fromaddr should be your gmail account, toaddrs can be anything
-fromaddr = ''
-toaddrs = ''
-msg = 'Hello Robert!'
+fromaddr = 'jack@example.com'
+toaddrs = 'jill@example.com'
+msg = 'Hello Jill!'
 
 #provide gmail user name and password
-username = ''
-password = ''
+username = 'your-email'
+password = 'your-password'
 
 # functions to send an email
 server = smtplib.SMTP('smtp.gmail.com:587')
